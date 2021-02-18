385fd2502ff92e085aba0bcb898795adcb384385
Jill Tsai <jilljane32@gmail.com>
2021-02-18 15:06:50 +0800

    一般交換路由CRUD

--- src/routes/normal-index.js
+++ src/routes/normal-index.js
@@ -3,14 +3,7 @@ const router = express.Router();
 const db = require(__dirname+'/../modules/db_connect');
 const app = express();
 
-// router.use((req, res, next)=>{
-//     if(!req.session.admin){
-//         return res.redirect('/');
-//     }
-//     res.locals.baseUrl = req.baseUrl;
-//     res.locals.url = req.url;
-//     next();
-// });
+
 
 // router.get('/',(req, res) => {
 //     res.send('normal-index測試');
@@ -24,7 +17,7 @@ const app = express();
 //   });
 
 
-//抓二手書一般交換資料&筆數頁數計算
+//抓二手書一般交換資料&筆數頁數計算(R)
   router.get("/", async (req, res)=>{
     const perPage = 10;
     const [t_rows] = await db.query("SELECT COUNT(1) num FROM `secondhand_normalchange`");
@@ -49,6 +42,56 @@ const app = express();
         rows,
     })
 });
+
+
+// add(C)
+router.post('/add', async (req, res) => {
+    const data = { ...req.body };
+    const sql = "INSERT INTO `secondhand_normalchange` set ?";
+    const [{ Rows, insertRows }] = await db.query(sql, [data]);
+
+    res.json({
+        success: !!Rows,
+        Rows,
+        insertRows
+    });
+})
+
+
+// 在POSTMAN用下面JSON資料測試ADD可以進資料庫，但success是false
+// {
+//     "ISBN":"9854621359453",
+//     "book_name":"測試5號",
+//     "book_condition":"5成新",
+//     "written_or_not":"有塗改",
+//     "member_sid_o":"15",
+
+// }
+
+
+//edit(U)
+router.post('/edit/:c_sid', async (req, res)=>{
+    const data = {...req.body};
+    const sql = "UPDATE `secondhand_normalchange` SET ? WHERE `c_sid`=?";
+    const [{Rows, changedRows}] = await db.query(sql, [ data, req.params.c_sid ]);
+
+    res.json({
+        success: !!changedRows,
+        Rows,
+        changedRows
+    });
+});
+
+
+// delete(D)
+router.delete('/delete/:c_sid', async (req, res) => {
+    const sql = "DELETE FROM `secondhand_normalchange` WHERE `c_sid`=?";
+    const [results] = await db.query(sql, [req.params.c_sid]);
+    res.json(results);
+})
+
+
+
 
 
 
