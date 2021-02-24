a0866e369017e12b52e9c28394e19b98335b8c25
jchans <jiangchenghans@gmail.com>
2021-02-25 02:57:59 +0800

    Sleeeeeep

--- src/routes/old-seasons.js
+++ src/routes/old-seasons.js
@@ -5,10 +5,14 @@ const db = require(__dirname+'/../modules/db_connect');
 const getSolarTerms = async(req) => {
   const output = {
     solar_term_list: [],
+    solar_term_books: [],
   }
 
   const [solarTerms] = await db.query("SELECT * FROM solar_terms ")
   output.solar_term_list = solarTerms;
+
+  const [solarTermsBooks] = await db.query("SELECT * FROM solar_term_books ")
+  output.solar_term_books = solarTermsBooks;
 
   return output
 }
