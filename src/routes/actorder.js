873fa6033330614dbda4b5c909a59e3a325772de
YU <j110456@gmail.com>
2021-02-27 05:07:10 +0800

    增加delete

--- src/routes/actorder.js
+++ src/routes/actorder.js
@@ -4,19 +4,27 @@ const db = require(__dirname + '/../modules/db_connect');
 
 // 活動訂單
 router.post('/', async(req, res)=>{
-    const output ={
-        success: false,
-        rows:[],
-    };
+    const sql = "SELECT `order`.`sid`,`order_detail`.`order_sid`,`order`.`member_sid`,`order`.`item_id`,`order`.`order_number`,`order_detail`.`bookname` ,`order_detail`.`act_time`,`order_detail`.`price`,`event`.`act_location`,`member`.`name`,`member`.`mobile`,`member`.`email`,`member`.`birthday`,`member`.`gender` FROM ( (`order_detail` INNER JOIN `order` ON `order_detail`.`order_sid`=`order`.`sid`) INNER JOIN `member`ON `member`.`sid` = `order`.`member_sid`)INNER JOIN `event` ON `event`.`act_name` = `order_detail`.`bookname` WHERE `order`.`member_sid`= ? "
 
-    const sql = "SELECT `order`.`member_sid`,`order`.`item_id`,`order`.`order_number`,`order_detail`.`bookname` ,`order_detail`.`act_time`,`order_detail`.`price`,`event`.`act_location`,`member`.`name`,`member`.`mobile`,`member`.`email`,`member`.`birthday`,`member`.`gender` FROM ( (`order_detail` INNER JOIN `order` ON `order_detail`.`order_sid`=`order`.`sid`) INNER JOIN `member`ON `member`.`sid` = `order`.`member_sid`)INNER JOIN `event` ON `event`.`act_name` = `order_detail`.`bookname` WHERE `order`.`member_sid`= ? "
-    // const sql = "SELECT `order`.`member_sid`,`order`.`item_id`,`order`.`order_number`,`order_detail`.`bookname` ,`order_detail`.`act_time`,`order_detail`.`price`,`event`.`act_location`,`member`.`name`,`member`.`mobile`,`member`.`email`,`member`.`birthday`,`member`.`gender` FROM ( (`order_detail` INNER JOIN `order` ON `order_detail`.`order_sid`=`order`.`sid`) INNER JOIN `member`ON `member`.`sid` = `order`.`member_sid`)INNER JOIN `event` ON `event`.`act_name` = `order_detail`.`bookname`"
     const sid = req.body.userId
     const [rows] = await db.query(sql, sid);
-    // const [rows] = await db.query(sql);
+
     // console.log('post', sql, sid )
     console.log('rows', rows )
+
     res.json({rows})
+});
+
+// 刪除報名
+router.delete('/:sid', async(req, res)=>{
+    const sql = "DELETE `order`, `order_detail` FROM `order`,`order_detail` WHERE `order`.`sid`=? AND`order_detail`.`order_sid`=?"
+
+    const [result] = await db.query(sql, [req.params.sid, req.params.sid]);
+
+    console.log('result', result)
+    res.json({
+        success: result.affectedRows === 2
+    })
 });
 
 module.exports = router;
