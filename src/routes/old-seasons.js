a93293517de06c9793d4d40205e5c7c0dceb782b
jchans <jiangchenghans@gmail.com>
2021-02-27 18:56:53 +0800

    新增取得初始節氣code

--- src/routes/old-seasons.js
+++ src/routes/old-seasons.js
@@ -6,13 +6,18 @@ const getSolarTerms = async(req) => {
   const output = {
     solar_term_list: [],
     solar_term_books: [],
+    current_solar_term_id: [],
   }
 
   const [solarTerms] = await db.query("SELECT * FROM solar_terms ")
   output.solar_term_list = solarTerms;
 
-  const [solarTermsBooks] = await db.query("SELECT * FROM solar_term_books ")
+  const [solarTermsBooks] = await db.query("SELECT solar_term_books.sid, solar_term_books.year, solar_term_books.solar_term_id, book_product.sid, book_product.book_pics, book_product.title FROM solar_term_books INNER JOIN book_product ON solar_term_books.book_id=book_product.sid")
   output.solar_term_books = solarTermsBooks;
+
+  const [currentSolarTermID] = await db.query("SELECT solar_term_books.sid, solar_term_books.year, solar_term_books.solar_term_id, book_product.sid, book_product.book_pics, book_product.title FROM solar_term_books INNER JOIN book_product ON solar_term_books.book_id=book_product.sid WHERE solar_term_books.year > NOW()")
+
+  output.current_solar_term_id = currentSolarTermID;
 
   return output
 }
